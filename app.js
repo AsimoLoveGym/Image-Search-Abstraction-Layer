61d0e4f44ef5b55182218f57024ba43dc9be4087
AsimoLoveGym <xiazhuangz@gmail.com>
2016-12-06 01:45:20 -0600

    [FEAT] Use google custom search API for service, also modified style and html.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var request = require('request');
 var app = express();
+
+var apiKey = 'AIzaSyBuuWG81dtb5JNRGccoSzyhxGCvS9CymSc';
+var cx = '006996082265588999635:yfupwd2xqso';
 
 var handlebars = require('express-handlebars').create({ defaultLayout: null });
 app.engine('handlebars', handlebars.engine);
@@ -13,15 +17,43 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.get('/new/*', function (req, res) {
-  var queryObject = req.path;
-  var queryString = queryObject.slice(5);
+app.get('/api/search?*', function (req, res) {
+  // var queryObject = req.path;
+  // var queryString = queryObject.slice(5);
+  // var outPutJson = {};
+  // res.json(outPutJsapion);
+
   var outPutJson = {};
-  res.json(outPutJson);
+  var queryTerm = req.query.q;
+  var apiUrl = 'https://www.googleapis.com/customsearch/v1?key=' + apiKey + '&cx=' + cx + '&q=' + queryTerm + '&searchType=image' + '&fields=items(link,snippet,image/thumbnailLink,image/contextLink)';
+  request(apiUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log(response);
+      // console.log(body);
+      // console.log(response.body.items);
+
+      // console.log(body);
+      outPutJson = JSON.parse(body);
+
+      // console.log(outPutJson)
+      // outPutJson = qs.parse(body);
+      // console.log(outPutJson.items);
+
+      // console.log(body[items]);
+      // outPutJson = JSON.stringify(body).items;
+      // res.json(outPutJson);
+      res.send('<pre>' + body + '</pre>');
+    }
+  });
+
+  // console.log(req.query.id);
+  // console.log(queryTerm);
+
+  // res.send('Welcome to search API');
 });
 
-app.get('/*', function (req, res) {
-  res.send('');
+app.get('/api/recent', function (req, res) {
+  res.send('Welcome to recent API');
 });
 
 // custom 404 page
